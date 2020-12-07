88b24e7f456b19696bf2635839a5d45485266b96
Trevor Bentley <snitch.el@x.mrmekon.com>
2020-12-08 00:30:11 +0100

    add a release script

---
+++ release.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -eux
+
+VERSION=$(cat snitch.el |grep ";; Version:" | sed 's/.*: //')
+PKG="snitch-$VERSION"
+
+sh test_snitch.sh
+if [[ $? -ne 0 ]]; then
+    echo "Automated tests failed, not releasing."
+    exit 1
+fi
+mkdir "$PKG"
+sh gen_readme.sh
+cp snitch*.el "$PKG"
+cp README.md "$PKG"
+tar -cf "$PKG.tar" "$PKG"
+
+if [[ "x$PKG" != "x" ]]; then
+    rm -rf "$PKG"
+fi
+echo "Released $PKG"
