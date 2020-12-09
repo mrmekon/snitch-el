8789294d84292b8e00928a35902c0175c1f0e993
Trevor Bentley <snitch.el@x.mrmekon.com>
2020-12-09 18:26:26 +0100

    make test script ignore snitch installed by package manager

--- test_snitch.sh
+++ test_snitch.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 emacs -batch \
-      --eval "(add-to-list 'load-path \"~/.emacs.d/snitch/\")" \
       --eval "(package-initialize)" \
+      --eval "(setq load-path (seq-filter \
+                (lambda (x) (not (string-match \"/snitch\" x))) load-path))" \
+      --eval "(add-to-list 'load-path \"~/.emacs.d/snitch/\")" \
+      --eval "(require 'snitch)" \
+      --eval "(message \"Testing snitch version: %s\" (snitch-version))" \
       -l ert -l snitch-test.el \
       -f ert-run-tests-batch-and-exit
